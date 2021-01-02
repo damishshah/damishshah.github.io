b6768d0492c84547fe74a4708b1b8af62d68199f
Damish <damish.shah@gmail.com>
2021-01-01 18:23:51 -0800

    Add game demo video and file formatting

--- js/main.js
+++ js/main.js
@@ -1,8 +1,10 @@
- //Get the button:
+//Get the button:
 mybutton = document.getElementById("topButton");
 
 // When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
+window.onscroll = function () {
+  scrollFunction();
+};
 
 function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
@@ -16,18 +18,18 @@ function scrollFunction() {
 function topFunction() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-} 
+}
 
 var overlayable = document.getElementsByClassName("overlayable");
-  
+
 for (var i = 0; i < overlayable.length; i++) {
-  overlayable[i].addEventListener("click", function(e) {
+  overlayable[i].addEventListener("click", function (e) {
     e.target.classList.toggle("overlay");
   });
 }
 
-document.addEventListener('click', function(e) {
-  var container = document.getElementsByClassName('overlay');
+document.addEventListener("click", function (e) {
+  var container = document.getElementsByClassName("overlay");
 
   for (var i = 0; i < container.length; i++) {
     if (!container[i].contains(e.target)) {
